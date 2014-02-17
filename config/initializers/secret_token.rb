5c6460b960932c7547b47f5e3110ee6e2fbd7415
derfsplat <derfsplat@github>
2014-02-17 18:48:33 -0500

    chore: initial commit !~!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bucky::Application.config.secret_key_base = '2f6a1142f419f5dfa12989c294f65060a7ee3a6a70af9065f7771dc9da6bce552f5fdd043df9cf62085cecfc1dea29c6aeb62c8844908fbc83eccbf2c783cdcf'
