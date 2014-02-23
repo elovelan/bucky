fd10061544dd12a9845364e3e2399938594bb2f7
somecallmejosh <josh@thebrileys.com>
2014-02-23 12:17:12 -0500

    Add styles for notifications and alerts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def flash_class(type)
+    case type
+    when :alert
+      "alert"
+    when :error
+        "error"
+    when :notice
+      "info"
+    else
+      ""
+    end
+  end
+  
 end
