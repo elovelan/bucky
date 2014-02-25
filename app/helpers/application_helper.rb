8ae2148a100646efd46525a11dac6f21800dd7da
somecallmejosh <josh@thebrileys.com>
2014-02-24 22:11:53 -0500

    Update layouts, refactor haml and sass.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def app_name
+    "ikick.it"
+  end
+
   def flash_class(type)
     case type
     when :alert
@@ -10,6 +14,19 @@ module ApplicationHelper
       "info"
     else
       ""
+    end
+  end
+
+  #- include svg files inside of templates, views, etc.
+  def inline_svg(file)
+    file = File.open("app/assets/images/svg/#{file}.svg", "rb")
+    raw file.read
+  end
+
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
     end
   end
   
