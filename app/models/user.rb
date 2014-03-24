afe43bc84bcd0f74a5dda9a44cf17eb765e60803
derfsplat <derfsplat@github>
2014-03-23 20:48:59 -0400

    wip(register): confirm via email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :full_name
+
+  belongs_to :kicker
+
+  def full_name
+    first_name + ' ' + last_name
+  end
+
+  def only_if_unconfirmed
+    pending_any_confirmation {yield}
+  end
 end
