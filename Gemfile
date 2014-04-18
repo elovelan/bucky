7c6c4af027a177e79f70e910609504c51ac522cf
derfsplat <derfsplat@github>
2014-04-17 22:54:13 -0400

    refactor(db): migreate to postgres for heroku

--- Gemfile
+++ Gemfile
@@ -3,7 +3,9 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
+#postgresql
+gem 'pg'
 
 gem 'profanity_filter'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140323204023) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "kickers", force: true do |t|
     t.string   "contact_email"
     t.string   "contact_phone"
@@ -55,8 +58,8 @@ ActiveRecord::Schema.define(version: 20140323204023) do
     t.datetime "confirmation_sent_at"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
